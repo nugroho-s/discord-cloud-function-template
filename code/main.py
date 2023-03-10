e67c49d79bb405edda97339b0892d2e40c1abe9d
nugsky <massatriya@gmail.com>
2023-03-11 00:21:19 +0700

    signature and reply
    Signed-off-by: nugsky <massatriya@gmail.com>

--- code/main.py
+++ code/main.py
@@ -7,21 +7,22 @@ def hello_world(name):
     return 'Hello {}!'.format(escape(name or "World"))
 
 def handle_interaction(request):
-     if request["type"] == 1:
+    if request["type"] == 1:
         return jsonify({
             "type": 1
         })
+    else:
+        return jsonify({
+            "type": 4,
+            "data": {
+                "content": "hello"
+            }
+        })
 
-def verify_signature(signature, timestamp):
+def verify_signature(body, signature, timestamp):
     # Your public key can be found on your application in the Developer Portal
-    PUBLIC_KEY = '<DISCORD PUBLIC KEY>'
-
+    PUBLIC_KEY = '011205304185c8e8c7e01ea97ec5d48888aeb9f1b1e054615a067a67c14c8b2a'
     verify_key = VerifyKey(bytes.fromhex(PUBLIC_KEY))
-
-    signature = request.headers["X-Signature-Ed25519"]
-    timestamp = request.headers["X-Signature-Timestamp"]
-    body = request.data.decode("utf-8")
-
     verify_key.verify(f'{timestamp}{body}'.encode(), bytes.fromhex(signature))
 
 @functions_framework.http
@@ -47,7 +48,7 @@ def hello_http(request):
         if (not "X-Signature-Ed25519" in request.headers) or (not "X-Signature-Timestamp" in request.headers):
             return 'invalid request signature', 401
         try:
-            verify_signature(request.headers["X-Signature-Ed25519"], request.headers["X-Signature-Timestamp"])
+            verify_signature(request.data.decode("utf-8"), request.headers["X-Signature-Ed25519"], request.headers["X-Signature-Timestamp"])
         except BadSignatureError:
             return 'invalid request signature', 401
         return handle_interaction(request_json)
